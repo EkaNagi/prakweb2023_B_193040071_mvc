11ccb1c07a83c3b3236bd6d708dcc177b8043fd9
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-10-23 18:42:03 +0700

    video 6

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2023_B_193040071_mvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-<
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/appaemo.png" alt="Appa Emo" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?php $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
