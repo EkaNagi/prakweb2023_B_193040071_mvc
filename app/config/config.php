803d5d29d4419b1590be18116599cc4622260c2e
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-10-23 19:46:06 +0700

    video 8

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2023_B_193040071_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
--- app/core/Constants.php
+++ app/core/Constants.php
@@ -1,3 +1,2 @@
 <?php
 
-define('BASEURL', 'http://localhost/prakweb2023_B_193040071_mvc/public');
